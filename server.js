2815d0627fe6c8e04be9e60749c70423e0932692
Gabriel Grego <gabrielsgrego@uol.com.br>
2024-09-11 21:56:06 -0300

    Criação do Banco de Dados e da Tabela

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 
-app.get('/',(req, res) => {
-    res.send('Hello World');
-});
+//cria um banco de dados SQLite em memoria
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('lista-tarefa.db');
+
+//cria a tabela 'tarefas' no bando de dados
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXIST tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)")
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
