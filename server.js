681bbc5468759778af381c4adb6b0939c99743a2
Gabriel Grego <gabrielsgrego@uol.com.br>
2024-09-11 22:31:17 -0300

    criada a rota para adicionar tarefas

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ app.use(express.json());
 
 //cria um banco de dados SQLite em memoria
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('lista-tarefa.db');
+const db = new sqlite3.Database(':memory:');
 
 //cria a tabela 'tarefas' no bando de dados
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXIST tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)")
+    db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)")
 })
+
+//rota para adicionar novas tarefas
+app.post('/tarefas', (req, res) => {
+    const { tarefa } = req.body;
+    db.run("INSERT INTO tarefas (tarefa) VALUES (?)", [tarefa], function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(201).json({ id: this.lastID, tarefa });
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
