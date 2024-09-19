4eb783fcb8612d0fe86d46de8e23f104b044e828
Gabriel Grego <gabrielsgrego@uol.com.br>
2024-09-18 22:47:29 -0300

    implementando cadastro de usuarios e autenticação

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const tarefaRoutes = require('./routes/tarefaRoutes');
+const userRoutes = require('./routes/userRoutes');
+const authMiddleware = require('./middlewares/authMiddleware');
 
 // Middleware para JSON
 app.use(express.json());
 
 // Rotas
-app.use('/api', tarefaRoutes);
+app.use('/api', userRoutes);
+
+// Rotas protegidas
+app.use('/api/tarefas', authMiddleware, tarefaRoutes);
 
 // Exporta o app para ser utilizado no server.js
 module.exports = app;
