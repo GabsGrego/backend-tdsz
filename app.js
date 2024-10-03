d08bfe11b3e3039348a0d8102b0abdafef2554c6
Gabriel Grego <gabrielsgrego@uol.com.br>
2024-10-02 22:33:13 -0300

    adicionando o CORS na aplicação

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors'); // Importa o CORS
 const app = express();
 const tarefaRoutes = require('./routes/tarefaRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -7,8 +8,11 @@ const authMiddleware = require('./middlewares/authMiddleware');
 // Middleware para JSON
 app.use(express.json());
 
+// Inicializa o CORS
+app.use(cors()); // Aqui é onde você deve inicializar o CORS
+
 // Rotas
-app.use('/api', userRoutes);
+app.use('/api', userRoutes); // Rota para autenticação
 
 // Rotas protegidas
 app.use('/api/tarefas', authMiddleware, tarefaRoutes);
